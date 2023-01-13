7eb171d267e2c5a2802f410483d0a0f84514b43f
Aman Ullah <aman.bangashqau@gmail.com>
2023-01-13 10:07:50 +0500

    deploy docker image v2

--- main.tf
+++ main.tf
@@ -36,7 +36,9 @@
    tags = {
      Name = "Terraform EC2 - using tls_private_key module"
    }
+   vpc_security_group_ids = [aws_security_group.maingroup.id]
    user_data = file("file.sh")
+   docker_compose_str = var.example_docker_compose
    
    #3. Connection Block-
    connection {
@@ -49,3 +51,43 @@
      timeout     = "4m"
    }
  }
+
+ resource "aws_security_group" "maingroup" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    }
+  ]
+}
